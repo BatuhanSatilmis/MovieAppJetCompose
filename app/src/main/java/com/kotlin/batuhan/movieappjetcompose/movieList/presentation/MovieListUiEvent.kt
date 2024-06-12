6bc702de9437625bb213df86d28988f3bcf19ce6
Batuhan Satilmis <batuhan.satilmis@arvato-scs.com>
2024-06-12 09:43:07 +0300

    hotfix: library fix

--- app/src/main/java/com/kotlin/batuhan/movieappjetcompose/movieList/presentation/MovieListUiEvent.kt
+++ app/src/main/java/com/kotlin/batuhan/movieappjetcompose/movieList/presentation/MovieListUiEvent.kt
@@ -2,6 +2,6 @@ package com.kotlin.batuhan.movieappjetcompose.movieList.presentation
 
 sealed interface MovieListUiEvent {
     data class Paginate(val category: String) : MovieListUiEvent
-    data object Navigate: MovieListUiEvent
+     object Navigate: MovieListUiEvent
 
 }
