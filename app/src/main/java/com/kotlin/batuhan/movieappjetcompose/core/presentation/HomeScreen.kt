ced82b69edb573afbaf0079917226fa579bedfd9
Batuhan Satilmis <batuhan.satilmis@arvato-scs.com>
2024-06-12 14:08:58 +0300

    add:home screen layouts and logics

--- app/src/main/java/com/kotlin/batuhan/movieappjetcompose/core/presentation/HomeScreen.kt
+++ app/src/main/java/com/kotlin/batuhan/movieappjetcompose/core/presentation/HomeScreen.kt
@@ -7,26 +7,37 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Movie
 import androidx.compose.material.icons.rounded.Upcoming
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.kotlin.batuhan.movieappjetcompose.R
 import com.kotlin.batuhan.movieappjetcompose.movieList.presentation.MovieListUiEvent
 import com.kotlin.batuhan.movieappjetcompose.movieList.presentation.MovieListViewModel
 import com.kotlin.batuhan.movieappjetcompose.movieList.util.Screen
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen( navController: NavHostController) {
       val movieListViewModel = hiltViewModel<MovieListViewModel>()
@@ -40,10 +51,38 @@ fun HomeScreen( navController: NavHostController) {
 
           },
           topBar = {
+             TopAppBar(
+                 title = {
+                     Text(
+                         text = if (movieState.isCurrentPopularScreen)
+                             stringResource(R.string.popular_movies)
+                         else
+                             stringResource(R.string.upcoming_movies),
+                         fontSize = 2.sp
+
+                     )
+                 },
+                 modifier = Modifier.shadow(2.dp),
+                 colors = topAppBarColors(
+                         MaterialTheme.colorScheme.inverseOnSurface,
+             ),
+
+             )
 
           }
       ){
-          Box(modifier = Modifier.padding(it))
+          Box(modifier = Modifier.padding(it)
+          ){
+              NavHost(navController = bottomNavController,
+                  startDestination = Screen.PopularMovieList.rout){
+                  composable(Screen.PopularMovieList.rout){
+                 //     PopularMovieScreen()
+                  }
+                  composable(Screen.PopularMovieList.rout){
+                 //     PopularMovieScreen()
+                  }
+              }
+          }
       }
 }
 
@@ -53,11 +92,11 @@ fun HomeScreen( navController: NavHostController) {
 fun BottomNavigationBar(bottomNavController : NavHostController, onEvent : (MovieListUiEvent) -> Unit){
     val items = listOf(
         BottomItem(
-            title = "Popular",
+            title = stringResource(R.string.popular),
             icon = Icons.Rounded.Movie
         ),
         BottomItem(
-            title = "Upcoming",
+            title = stringResource(R.string.upcoming),
             icon = Icons.Rounded.Upcoming
         )
     )
