3e395a1e21baef60dfb9890f779362f788bd7cd2
Batuhan Satilmis <batuhan.satilmis@arvato-scs.com>
2024-06-13 14:20:27 +0300

    fix : searchbar

--- app/src/main/java/com/kotlin/batuhan/movieappjetcompose/movieList/presentation/components/SearchBar.kt
+++ app/src/main/java/com/kotlin/batuhan/movieappjetcompose/movieList/presentation/components/SearchBar.kt
@@ -2,12 +2,17 @@ package com.kotlin.batuhan.movieappjetcompose.movieList.presentation.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
     searchQuery: String,
@@ -18,8 +23,13 @@ fun SearchBar(
         onValueChange = onSearchQueryChanged,
         modifier = Modifier.fillMaxWidth().padding(16.dp),
         placeholder = {
-            Text(text = "Search.....")
+            Text(text = "Search it")
         },
-        singleLine = true
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+            focusedTextColor = Color.Cyan,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
     )
 }
